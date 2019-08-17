fc50c9d5a3cb696c69835cceaf1ad7140e168f49
AlexeyRadchenko <novame@mail.ru>
2019-08-17 16:31:07 +0500

    create podcast upload form class

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,9 @@
-# Here will be Flask Web Forms
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+
+class FileUploadForm(FlaskForm):
+    file = FileField('Podcast file', validators=[
+        FileRequired(),
+        FileAllowed(['mp3'], "Wrong format! Only mp3 format audio files")
+    ])
