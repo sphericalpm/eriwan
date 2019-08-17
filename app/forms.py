79f112c1297261e77352f727cd4087e87e603bfc
chuker <chuker69@mail.com>
2019-08-17 19:04:53 +0300

    add: add_joke template form for jokes routes

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
-# Here will be Flask Web Forms
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class UploadJokeForm(FlaskForm):
+    text = StringField('Текст', validators=[DataRequired()])
+    submit = SubmitField('Сохранить')
