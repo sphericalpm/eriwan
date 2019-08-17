9049d653c1ceb5e88dd3a17f08dcd5b6b07c23ec
Roman <namor925@gmail.com>
2019-08-17 20:24:56 +0700

    create file

---
+++ random_jokes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+
+import random
+from models import Joke
+from flask_sqlalchemy import SQLAlchemy
+
+def get_all_id():
+    pass
+
+
+def random_jokes():
+    pass
