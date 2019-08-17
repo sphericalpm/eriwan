90a736fd09a096bd5ed5fd0b80aa7c652f0ca4a3
lex51 <aaaalex51@yandex.ru>
2019-08-17 19:46:03 +0300

    Delete routes.py

--- app/routes.py
+++
@@ -1,9 +0,0 @@
-# View functions
-from flask import render_template
-from app import app
-
-
-@app.route('/')
-def index():
-    feed_blank = 'Podcast Main page: RSS feed'
-    return render_template('index.html', feed_blank=feed_blank)
