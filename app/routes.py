f4188354bc2c801fb7df9e8e108a153663f97a0d
AlexeyRadchenko <novame@mail.ru>
2019-08-17 17:07:45 +0500

    add upload route

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,18 @@
 # View functions
 from flask import render_template
 from app import app
+from .forms import FileUploadForm
 
 
 @app.route('/')
 def index():
     feed_blank = 'Podcast Main page: RSS feed'
     return render_template('index.html', feed_blank=feed_blank)
+
+
+@app.route('/upload-podcast/', methods=['POST'])
+def upload_file():
+    form = FileUploadForm(request.form)
+    if form.validate():
+        pass
+    return render_template('index.html', form=form)
