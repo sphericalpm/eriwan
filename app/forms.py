4ac1e6004c960e1b3fb36bcbdaaa3eeb70281ce7
Molfar <vladuslav09@gmail.com>
2019-08-17 17:50:12 +0300

    add user login/logout function fix

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 # Here will be Flask Web Forms
-from flask_wtf import  FlaskForm
-from wtforms import  StringField, PasswordField, BooleanField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 
