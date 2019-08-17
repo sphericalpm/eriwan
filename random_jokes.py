e6cbf96018ff942b15d2d16988fae219ba7a0e2b
Roman <namor925@gmail.com>
2019-08-18 02:46:25 +0700

    before clean

--- random_jokes.py
+++ random_jokes.py
@@ -2,9 +2,26 @@
 # -*- coding: utf-8 -*-
 #
 
-from random import shuffle
+from random import choice, randint
 from app.models import Joke
+from app import db
 
+
+def get_max_id():
+    ''' return max id from table'''
+    return db.session.query(db.func.max(Joke.id)).scalar()
+
+max_id = get_max_id()
+
+def generate_random_int(max_id=max_id):
+    for x in range(1): 
+        #print(randint(1,max_id))
+        return randint(1,max_id)
+
+rand_int = generate_random_int()
+
+def get_text(rand_id=rand_int):
+    return Joke.query.filter_by(id=rand_id).first().joke_text
 
 def random_joke():
     ''' this is function return one random id'''
@@ -13,15 +30,15 @@ def random_joke():
     if len(jokes) == 0:
         print('database is empty!')
     else:
+        #b = Joke.query.filter_by(id='1').first()
         for i in jokes:
-            all_jokes_id.append(i.id)
+            all_jokes_id.append(i.joke_text)
 
-    shuffle(all_jokes_id)
-    random_id = all_jokes_id[0]
-    a = Joke.query.filter_by(id=random_id).first()
-    #print(a.joke_text)
-    return a.joke_text
+    #return b.id
+    return choice(all_jokes_id)
 
 
 if __name__ == '__main__':
-    random_joke()
+    #a = random_joke()
+    a = get_text()
+    print(a)
