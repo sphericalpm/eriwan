fdd22877615fd8b2b5674acee32a217383f676f7
Andrey Anokhin <anokhin.andrey@gmail.com>
2019-08-17 13:04:58 +0300

    Basic structure of flask-app

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+# Creates the application object as an instance of class Flask
+
+
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+
+# using Config class from ./config.py
+app.config.from_object(Config)
+
+# The routes module is imported at the bottom and not at the top of the script
+# as it is always done. The bottom import is a workaround to circular imports,
+# a common problem with Flask applications.
+from app import routes, models
---
+++ app/models.py
@@ -0,0 +1,4 @@
+# Here will be Classes with model definitions
+
+# from app import app, db
+# Here will be db models
