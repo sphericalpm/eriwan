8a3612606b055737a558206e075359b3e4c921a8
chuker <chuker69@mail.com>
2019-08-17 20:18:24 +0300

    pep8 fix

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+
 from config import Config
 
 app = Flask(__name__)
