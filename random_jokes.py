22acadf576e3d027277aa3fddcaf864b4a5c0c2c
Roman <namor925@gmail.com>
2019-08-17 21:31:26 +0700

    add check

--- random_jokes.py
+++ random_jokes.py
@@ -10,8 +10,11 @@ def random_joke():
     ''' this is function return one random id'''
     all_jokes_id = []
     jokes = Joke.query.all()
-    for i in jokes:
-        all_jokes_id.append(i.id)
+    if len(jokes) == 0:
+        print('database is empty!')
+    else:
+        for i in jokes:
+            all_jokes_id.append(i.id)
 
     shuffle(all_jokes_id)
     return all_jokes_id[0]
