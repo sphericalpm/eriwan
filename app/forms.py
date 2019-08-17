1616ed0dac0211b9d2be403b206e432dc91f8019
AlexeyRadchenko <novame@mail.ru>
2019-08-17 18:19:15 +0500

    create form template and form validation

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 
 class FileUploadForm(FlaskForm):
-    file = FileField('Podcast file', validators=[
+    file = FileField('Upload podcast', validators=[
         FileRequired(),
         FileAllowed(['mp3'], "Wrong format! Only mp3 format audio files")
     ])
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 # View functions
-from flask import render_template
+from flask import render_template, redirect, url_for
+from werkzeug.datastructures import CombinedMultiDict
+from flask import request
+
 from app import app
 from .forms import FileUploadForm
 
@@ -7,12 +10,14 @@ from .forms import FileUploadForm
 @app.route('/')
 def index():
     feed_blank = 'Podcast Main page: RSS feed'
-    return render_template('index.html', feed_blank=feed_blank)
+    form = FileUploadForm()
+    return render_template('index.html', feed_blank=feed_blank, form=form)
 
 
-@app.route('/upload-podcast/', methods=['POST'])
+@app.route('/upload-podcast', methods=['POST'])
 def upload_file():
-    form = FileUploadForm(request.form)
+    form = FileUploadForm(CombinedMultiDict((request.files, request.form)))
     if form.validate():
-        pass
+        return redirect(url_for('index'))
     return render_template('index.html', form=form)
+
