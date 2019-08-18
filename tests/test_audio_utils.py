a69d9c6e134e0c4b725de85488b25faef1f1f2f5
Turkova Maria <kvm1990@mail.ru>
2019-08-18 11:47:28 +0300

    empty file

--- tests/test_audio_utils.py
+++
@@ -1,8 +0,0 @@
-
-if __name__ == "__main__":
-    from app.audio_utils import text_to_speech, concatenate_audios
-    speech = text_to_speech("Всем привет")
-    concatenate_audios(
-        ["static/jingles/jingle.mp3", speech, "static/jingles/jingle.mp3"],
-        "episode",
-        "3")
