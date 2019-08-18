f87acff80d9f59c9e98b439b8c26cd1126c7d52c
Turkova Maria <kvm1990@mail.ru>
2019-08-18 15:51:47 +0300

    correct tests

--- tests/test_audio_utils.py
+++ tests/test_audio_utils.py
@@ -1,25 +1,50 @@
+import pytest
+import os.path as path
+import os
+import struct
+
 from app.audio_utils import (
     text_to_speech,
-    check_and_create_file_dir,
     concatenate_audios
 )
 
+
 def test_text_to_speech():
-    pass
-    with open(abs_graph_path, 'rb') as f1, \
-        open(abs_test_graph_path, 'rb') as f2:
+    speech_path = text_to_speech("Всем привет")
+
+    script_path = path.abspath(path.join(os.getcwd()))
+    test_audio_path = os.path.join(script_path, "tests", "test_data", 'test_audio.mp3')
+
+    with open(speech_path, 'rb') as f1, \
+        open(test_audio_path, 'rb') as f2:
             while True:
-            b1, b2 = f1.read(1), f2.read(1)
-            if not b1 or not b2:
-                break
-            i = struct.unpack('B', b1)[0] - struct.unpack('B', b2)[0]
+                b1, b2 = f1.read(1), f2.read(1)
+                if not b1 or not b2:
+                    break
+                i = struct.unpack('B', b1)[0] - struct.unpack('B', b2)[0]
 
-            assert i == 0
-
-
-def check_and_create_file_dir():
-    pass
+                assert i == 0
 
 
-def concatenate_audios():
-    pass
+def test_concatenate_audios():
+    script_path = path.abspath(path.join(os.getcwd()))
+    test_audio_path = os.path.join(script_path, "tests", "test_data", 'test_audio_with_jingle.mp3')
+    
+    concatenate_audio_path = os.path.join(script_path, "tests", "test_data",  "jingle_and_joke.mp3")
+    voice_from_text = text_to_speech("Всем привет")
+    jingle = "app/static/jingles/jingle.mp3"
+
+    concatenate_audios(
+        [jingle, voice_from_text, jingle],
+        concatenate_audio_path
+    )
+
+    with open(concatenate_audio_path, 'rb') as f1, \
+        open(test_audio_path, 'rb') as f2:
+            while True:
+                b1, b2 = f1.read(1), f2.read(1)
+                if not b1 or not b2:
+                    break
+                i = struct.unpack('B', b1)[0] - struct.unpack('B', b2)[0]
+
+                assert i == 0
