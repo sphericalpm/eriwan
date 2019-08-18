45468ba7fb92378cde452daec8a0a4f02e8f2a3c
lex51 <aaaalex51@yandex.ru>
2019-08-18 11:48:17 +0300

    Update parser.py

--- app/parser.py
+++ app/parser.py
@@ -1,5 +1,4 @@
 # parsing jokes from anekdotitut.ru
-# 
 import urllib.request
 from urllib.parse import quote
 from urllib.parse import unquote
@@ -11,18 +10,15 @@ from app import db
 
 def parse_anekdot():
     jokes_out = []
-    for i in range(1, 10):
+    for i in range(1, 11):
         url = url = 'https://anekdotitut.ru/pro_armyanskoe_radio' + str(
             i) + '.php'
         html_doc = urllib.request.urlopen(url)
         soup_doc = BeautifulSoup(html_doc, 'html.parser')
         jokes = soup_doc.body(class_='noselect', id=re.compile('anekdot\d+'))
         for joke in jokes:
-            jokes_out.append(joke.text)
-            if not bool(Joke.query.filter_by(joke_text='some_text').first()):
-                j = Joke(joke_text=joke.text)
+            # jokes_out.append(joke.text)
+            if not bool(Joke.query.filter_by(joke_text = joke.text).first()):
+                j = Joke(joke_text = joke.text, user_id = 999)
                 db.session.add(j)
                 db.session.commit()
-
-    # return jokes_out
-
