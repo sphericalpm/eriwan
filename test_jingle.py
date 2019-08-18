a99df1cd69aeee7f763d069702f9a08c6a63e757
Turkova Maria <kvm1990@mail.ru>
2019-08-18 11:47:07 +0300

    changed path

--- test_jingle.py
+++ test_jingle.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from app.audio_utils import text_to_speech, concatenate_audios
     speech = text_to_speech("Всем привет")
     concatenate_audios(
-        ["static/jingles/jingle.mp3", speech, "static/jingles/jingle.mp3"],
-        "episode",
+        ["app/static/jingles/jingle.mp3", speech, "app/static/jingles/jingle.mp3"],
+        "jokes",
         "3")
