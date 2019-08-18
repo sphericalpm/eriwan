5a14839edfbd7fda393c1b9833a21e2b2b89bc4d
chuker <chuker69@mail.com>
2019-08-18 14:29:50 +0300

    add comments

--- app/forms.py
+++ app/forms.py
@@ -55,5 +55,11 @@ class LoginForm(FlaskForm):
 
 
 class UploadJokeForm(FlaskForm):
-    text = StringField('Текст', validators=[DataRequired()])
+    """
+    Form for Jokes:
+    :parameter:
+    text - Joke text;
+    submit - submit button;
+    """
+    text = StringField('Текст шутки', validators=[DataRequired()])
     submit = SubmitField('Сохранить')
